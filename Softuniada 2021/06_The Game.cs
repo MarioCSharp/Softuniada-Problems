a3cdc511d0dc4e92da02bdbc3ee0f817b3dabdc8
Mario Petkov <mario.18@abv.bg>
2022-03-04 18:54:51 +0200

    some new problems

---
+++ Softuniada 2021/06_The Game.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp14
+{
+    using System;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            string word = Console.ReadLine();
+            string toConvertTo = Console.ReadLine();
+            if (IsTransformable(word, toConvertTo))
+            {
+                Console.WriteLine($"The minimum operations required to convert \"{word}\" to \"{toConvertTo}\" are {CalucateMinimumOperations(word, toConvertTo)}");
+            }
+            else
+            {
+                Console.WriteLine("The name cannot be transformed!");
+            }
+        }
+        public static bool IsTransformable(string first, string second)
+        {
+            if (first.Length != second.Length)
+            {
+                return false;
+            }
+            return string.Concat(first.OrderBy(x => x)) == string.Concat(second.OrderBy(x => x));
+        }
+        public static int CalucateMinimumOperations(string first, string second)
+        {
+            int count = 0;
+            for (int i = first.Length - 1, j = second.Length - 1; i >= 0; i--, j--)
+            {
+                while (i >= 0 && first[i] != second[j])
+                {
+                    i--;
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
